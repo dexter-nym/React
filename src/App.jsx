6b11258acc70af96ee601908f73dce27a48b339e
Kunal Paraye <kunalparaye39@gmail.com>
2025-02-18 23:18:50 +0530

    card -> app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Food from "./components/Food";
+// Components
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Food from "./components/Food";
+// function App() {
+//   return (
+//     <>
+//       <Header />
+//       <Food />
+//       <Footer />
+//     </>
+//   );
+// }
+// export default App;
+
+// Card Component
+import Card from "./Card";
 
 function App() {
   return (
     <>
-      <Header />
-      <Food />
-      <Footer />
+      <Card />
     </>
   );
 }
