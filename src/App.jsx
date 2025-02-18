30c1c2ac03c7c3531380c9e336af89eab01e7b75
Kunal Paraye <kunalparaye39@gmail.com>
2025-02-18 23:14:20 +0530

    components header footer food moved in components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from "./Header";
-import Footer from "./Footer";
-import Food from "./Food";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Food from "./components/Food";
 
 function App() {
   return (
